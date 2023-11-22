60a5adfcc4bffded55d593afb2ae75ea50c6dfd5
GanSii <rudolfwebster.cruz@gmail.com>
2023-11-22 19:58:22 +0800

    first commit

---
+++ assets/scripts/scripts.js
@@ -0,0 +1,8 @@
+const body = document.body;
+const div = document.createElement("div");
+const strong = document.createElement("strong");
+
+strong.innerText = "Hello Sun";
+
+body.append(div);
+div.append(strong);
